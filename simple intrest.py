e626c7cb4a7943ecb638c03ca02c8fe49a6d38a8
jayanth12344 <112242706+jayanth12344@users.noreply.github.com>
2022-08-28 17:23:26 +0530

    Add files via upload

---
+++ simple intrest.py
@@ -0,0 +1,5 @@
+principle_amount=int(input("enter the value of principle"))
+rate_of_intrest=int(input("enter the value of intrest"))
+time_span=int(input("enter the value time span"))
+simple_intrest=(principle_amount *rate_of_intrest *time_span)/100
+print(simple_intrest)
---
+++ temp.py
@@ -0,0 +1,4 @@
+farenheit=int(input("enter the value :"):
+              celsius=((farenheit-32)*5)/9)
+print(" celsius temperature is :")
+print(celsius)
