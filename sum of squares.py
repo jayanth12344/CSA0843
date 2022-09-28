ed85320d0ccf8a9dc040ffadae20a93cd010f8ae
jayanth12344 <112242706+jayanth12344@users.noreply.github.com>
2022-09-28 14:33:39 +0530

    Add files via upload

--- sum of squares.py
+++ sum of squares.py
@@ -1,14 +1,22 @@
-sk=[]
-n=int(input("number of elements are :"))
-for i in range(0,n):
-    print("the number :")
-    item=int(input())
-    sk.append(item)
-g=[]
-h=[]
-for i in sk:
-    if i%2==0:
-        g.append(i*i)
-    else:
-        h.append(i*i)
-print("[",sum(h),sum(g),"]")
+myList= []
+length = int(input("enter the number of elements:"))
+even=0
+odd=0
+for i in range(0,length):
+    value =int(input())
+    myList.append(value)
+print("even numbers are:")
+for element in myList:
+    if element%2 == 0:
+        print(element)
+        even =even+(element**2)
+
+print("odd numbers are:")
+for element in myList:
+    if element%2 != 0:
+        print(element)
+        odd = odd+(element**2)
+print("even sum square is :",even)
+print("odd sum square is :",odd)
+        
+
