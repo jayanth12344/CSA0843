c9a9c1d5f41910182c7bc5189a6eb85e9d053301
jayanth12344 <112242706+jayanth12344@users.noreply.github.com>
2022-09-29 13:33:51 +0530

    Add files via upload

---
+++ vowels.py
@@ -0,0 +1,12 @@
+str1=input("enter the string:")
+vowels=0
+for i in str1:
+     if(i == 'a' or i == 'e' or i == 'i' or i == 'o' or i == 'u' or i == 'A'
+       or i == 'E' or i == 'I' or i == 'O' or i == 'U'):
+         vowels = vowels + 1
+print("total number of vowels in a string: ",vowels)         
+         
+        
+    
+
+    
